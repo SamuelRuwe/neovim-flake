605a4319f3dd5c5c547a58ab7bdeabe1782d505b
SamuelRuwe <ruwesam@gmail.com>
2024-12-08 12:08:09 -0500

    feat: Add additional flash mapping

--- nvim/plugin/flash.lua
+++ nvim/plugin/flash.lua
@@ -1,1 +1,10 @@
-require('flash').setup {}
+local flash = require('flash')
+flash.setup {}
+
+vim.keymap.set('n', 's', function()
+  flash.jump()
+end, { desc = 'Flash' })
+
+vim.keymap.set('i', '<c-s>', function()
+  flash.jump()
+end, { desc = 'Flash' })
