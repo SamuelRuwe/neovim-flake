f0fe3e1477792cb4c928c3e1b078a0b7c15fd058
SamuelRuwe <ruwesam@gmail.com>
2025-05-25 13:27:33 -0400

    feat: Update conform config

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -8,12 +8,17 @@ require('conform').setup {
     javascriptreact = { 'prettierd', 'prettier', lsp_format = 'fallback', stop_after_first = true },
     typescript = { 'prettierd', 'prettier', lsp_format = 'fallback', stop_after_first = true },
     typescriptreact = { 'prettierd', 'prettier', lsp_format = 'fallback', stop_after_first = true },
+    haskell = { lsp_format = 'fallback' },
+  },
+  format_on_save = {
+    lsp_format = 'fallback',
+    timeout_ms = 500,
   },
 }
 
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*',
-  callback = function(args)
-    require('conform').format { bufnr = args.buf }
-  end,
-})
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   pattern = '*',
+--   callback = function(args)
+--     require('conform').format { bufnr = args.buf }
+--   end,
+-- })
